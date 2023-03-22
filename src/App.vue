04d178f0a49ba3a8a7d6043b96186035d0fbca98
gui.dani13@gmail.com <gui.dani13@gmail.com>
2023-03-22 10:46:11 -0300

    criado layout básico

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { computed, onMounted, ref, watch } from "vue";
 import AddIcon from "./components/AddIcon.vue";
+import RootLayout from "./components/layouts/RootLayout.vue";
 import ListItem from "./components/ListItem.vue";
 import { store } from "./stores/store";
 
@@ -37,48 +38,48 @@ onMounted(() => {
 
 <template>
   <div class="container mx-auto">
-    <div class="flex flex-col gap-2">
-      <div class="flex justify-between">
-        <h2 class="text-2xl">{{ title }}</h2>
-        <p>Criado com ajuda do ChatGPT</p>
-      </div>
-      <form @submit.prevent="store.add()" class="flex gap-2">
-        <input
-          type="text"
-          v-model="store.newTodo"
-          placeholder="Add a new todo..."
-          class="outline-1 outline-slate-900 border pl-2 py-2 flex-1"
-          ref="myInput"
-        />
+      <RootLayout>
+      <div class="flex flex-col gap-2">
+        <form @submit.prevent="store.add()" class="flex gap-2">
+          <input
+            type="text"
+            v-model="store.newTodo"
+            placeholder="Add a new todo..."
+            class="outline-1 outline-slate-900 border pl-2 py-2 flex-1"
+            ref="myInput"
+          />
+          <button
+            type="submit"
+            class="bg-green-700 w-20 text-center rounded-md text-white text-6xl flex justify-center items-center"
+          >
+            <AddIcon />
+          </button>
+        </form>
+        <p v-if="store.todos.length === 0">
+          Adicione uma tarefa para continuar!
+        </p>
         <button
-          type="submit"
-          class="bg-green-700 w-20 text-center rounded-md text-white text-6xl flex justify-center items-center"
+          @click="hideCompleted = !hideCompleted"
+          v-if="store.todos.length !== 0"
+          class="toggle"
         >
-          <AddIcon />
+          {{ hideCompleted ? "Exibir todos" : "Ocultar completos" }}
         </button>
-      </form>
-      <p v-if="store.todos.length === 0">Adicione uma tarefa para continuar!</p>
-      <button
-        @click="hideCompleted = !hideCompleted"
-        v-if="store.todos.length !== 0"
-        class="toggle"
-      >
-        {{ hideCompleted ? "Exibir todos" : "Ocultar completos" }}
-      </button>
-      <ul>
-        <ListItem
-          v-for="(todo, index) in filteredTodos"
-          :done="todo.done"
-          :index="index"
-          :text="todo.text"
-          v-model="todo.done"
-          @remove-todo="store.remove(index)"
-          @update-todo="store.update(index, todo.text)"
-          @toggle-todo="store.toggle(index)"
-        />
-      </ul>
+        <ul>
+          <ListItem
+            v-for="(todo, index) in filteredTodos"
+            :done="todo.done"
+            :index="index"
+            :text="todo.text"
+            v-model="todo.done"
+            @remove-todo="store.remove(index)"
+            @update-todo="store.update(index, todo.text)"
+            @toggle-todo="store.toggle(index)"
+          />
+        </ul>
+      </div>
+    </RootLayout>
     </div>
-  </div>
 </template>
 
 <style scoped>
---
+++ src/components/layouts/RootLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-between my-2">
+    <h2 class="text-2xl">Lista de tarefas</h2>
+    <p>Criado com ajuda do ChatGPT</p>
+  </div>
+  <slot></slot>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
